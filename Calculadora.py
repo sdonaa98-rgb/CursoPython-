bd56237a41938947149b32fadacd847cf4648b39
Donaji <sdonaa98@gmail.com>
2026-01-13 21:21:59 -0600

    Feat:Add_file_operaciones

---
+++ Calculadora.py
@@ -0,0 +1,15 @@
+#Script tendra operaciones aritmeticas 
+print ("Calculadora")
+numeroA = int(input("Escribe un numero: "))
+numeroB = int(input("Escribe un numero: "))
+Resultado= numeroA+numeroB
+print (f"Este es tu resultado de suma: {Resultado}")
+#Operación Resta
+Resultado= numeroA-numeroB
+print (f"Este es tu resultado de resta: {Resultado}")
+#Operación Multiplación
+Resultado= numeroA*numeroB
+print (f"Este es tu resultado de multiplicar: {Resultado}")
+#operación División
+Resultado= numeroA/numeroB
+print (f"Este es tu resultado de dividir: {Resultado}")
--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,15 @@
 print("¡Hola mundo!")
+nombre = input("¿Cómo te llamas? ")
+edad = int(input("Edad: "))
+print (nombre)
+print (edad)
+#print(f"Hola {nombre}, tienes {edad} años")
+if edad >= 65:  
+    print ("adulto mayor")
+elif edad >=18 and edad <=64:
+    print ("Mayor de edad")
+elif edad <=17 and edad >=1:
+    print("Menor de edad")
+else:
+    print ("numero negativo")
+    
